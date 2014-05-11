585473125561ea6ef5f7c16b577ed93f277f2aa1
Dom De Re <domdere@domdere.com>
2014-05-11 23:14:03 +1000

    Finished definition of Adjunctions

--- src/classes/Adjunction.v
+++ src/classes/Adjunction.v
@@ -13,10 +13,26 @@ Class Adjunction (L R : Type -> Type) := {
     (* laws/properties *)
 
     adjoint_bijection_1
-        : forall {A B : Type} (g : A -> R B) (a : A), (leftAdjoint (rightAdjoint g)) a = g a;
+        : forall {A B : Type} (g : A -> R B) (a : A)
+        , (leftAdjoint (rightAdjoint g)) a = g a;
 
     adjoint_bijection_2
-        : forall {A B : Type} (f : L A -> B) (la : L A), (rightAdjoint (leftAdjoint f)) la = f la
+        : forall {A B : Type} (f : L A -> B) (la : L A)
+        , (rightAdjoint (leftAdjoint f)) la = f la;
 
+    adjoint_natural_1
+        : forall {A B C : Type} (f : L A -> B) (k : B -> C) (a : A)
+        , (leftAdjoint (compose k f)) a = fmap k (leftAdjoint f a);
 
+    adjoint_natural_2
+        : forall {A B C : Type} (f : L B -> C) (h : A -> B) (a : A)
+        , (leftAdjoint (compose f (fmap h))) a = (leftAdjoint f (h a));
+
+    adjoint_natural_3
+        : forall {A B C : Type} (g : B -> R C) (h : A -> B) (la : L A)
+        , (rightAdjoint (compose g h)) la = (rightAdjoint g (fmap h la));
+
+    adjoint_natural_4
+        : forall {A B C : Type} (g : A -> R B) (k : B -> C) (la : L A)
+        , rightAdjoint (compose (fmap k) g) la = k (rightAdjoint g la)
 }.
